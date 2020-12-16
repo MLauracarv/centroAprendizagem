f118e985b7990a17f69342ef3770daf0636689f7
joseroldao27062002 <59697831+joseroldao27062002@users.noreply.github.com>
2020-12-16 06:09:16 -0300

    Add files via upload

---
+++ app/models/Formulario.java
@@ -0,0 +1,9 @@
+package models;
+import java.util.ArrayList;
+
+import controllers.Questoes;
+import play.db.jpa.Model;
+
+public class Formulario extends Model {
+	public ArrayList<Questoes> questao;
+}
