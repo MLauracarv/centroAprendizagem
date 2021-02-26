60e922233e273fa24b2cc0615072bdc61a281459
joseroldao27062002 <59697831+joseroldao27062002@users.noreply.github.com>
2021-02-26 19:22:22 -0300

    Update Tabuleiros.java

--- app/controllers/Tabuleiros.java
+++ app/controllers/Tabuleiros.java
@@ -9,12 +9,23 @@ public class Tabuleiros extends Controller {
 	}
 	
 	public static void formTabuleiro(Long salaVirtual) {
-		render();
+		Tabuleiro tabuleiro1 = Tabuleiro.find("idSalaVirtual = ?", salaVirtual).first();
+		if(tabuleiro1 == null ) {
+			System.out.println("");
+			System.out.println("ESSE TABULEIRO NAO EXISTEEEE ");
+			System.out.println("");
+		}
+		else {
+			System.out.println("Esse tabuleiro existe. n crie outro!");
+			indexTabuleiro();
+		}
+		render(salaVirtual);
 	}
 	
 	public static void salvarTabuleiro(Tabuleiro t, Long idSala) {
 		SalaVirtual sala = SalaVirtual.findById(idSala);
 		t.salaVirtual = sala;
 		t.save();
+		renderTemplate("Tabuleiros/indexTabuleiro.html");
 	}
 }	
